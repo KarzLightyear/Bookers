268b51e1f7b84ae3dc4ab5e2d1f4e6f973525c4d
KarzLightyear <k.k.yamato@gmail.com>
2021-06-04 08:25:55 +0000

    [update]課題問題フラッシュメッセージまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,25 +7,38 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created'
+      redirect_to book_path(@book)
+    end
+
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to show_book_path(book.id)
-  end  
-  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated'
+      redirect_to book_path(@book)
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = 'Book was successfully destroyed'
+      redirect_to books_path
+    end
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
-  end  
+  end
 end
