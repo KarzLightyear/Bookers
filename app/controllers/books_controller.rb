8d7e26a322669c3cdbf805f2f75d69598bf4a096
KarzLightyear <k.k.yamato@gmail.com>
2021-06-05 09:16:47 +0000

    [update]課題問題バリデーション途中2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,8 @@ class BooksController < ApplicationController
       flash[:notice] = 'Book was successfully created'
       redirect_to book_path(@book)
     else
-      render action: :index
+      @books = Book.all
+      render :index
     end
 
   end
